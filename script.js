803cbc4c23cc5a3c0102d8e9f51c3e77c9964322
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-03-27 23:27:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+    inputs.forEach((input, index) => {
+        input.addEventListener('input', (e) => {
+            const val = e.target.value;
+            
+            if (val.length === 1 && index < 5) {
+                inputs[index + 1].focus();
+            }
+        });
+
+        input.addEventListener('keydown', (e) => {
+            if (e.key === 'Backspace' && index > 0) {
+                inputs[index - 1].focus();
+            }
+        });
+    });
